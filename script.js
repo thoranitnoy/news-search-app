f5dc69e090091be6f97c3101ddac351759bde0f4
thoranitnoy <137036743+thoranitnoy@users.noreply.github.com>
2024-09-24 15:44:39 +0700

    Create search api

---
+++ script.js
@@ -0,0 +1,46 @@
+const apiKey = "cb9a8eea891b4b838c81c69fd717f0eb";
+
+const blogContainer = document.getElementById("blog-container");
+
+async function fetchRandomNews(){
+    try{
+        const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apiKey=${apiKey}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data.articles;
+    }catch(error){
+        console.error("Error fetching random news",error);
+        return [];
+    }
+}
+
+function displayBlogs(articles){
+    blogContainer.innerHTML = ""
+    articles.forEach((article)=>{
+        const blogCard = document.createElement("div");
+        blogCard.classList.add("blog-card");
+        const img = document.createElement("img");
+        img.src = article.urlToImage;
+        img.alt =article.title;
+        const title = document.createElement("h2");
+        title.textContent = article.title;
+        const description = document.createElement("p");
+        description.textContent = article.description;
+
+        blogCard.appendChild(img);
+        blogCard.appendChild(title);
+        blogCard.appendChild(description);
+        blogContainer.appendChild(blogCard);
+    })
+}
+
+(async ()=>{
+    try{
+       const articles = await fetchRandomNews();
+    displayBlogs(articles);
+    }catch(error){
+        console.error("Error fetching random news",
+            error
+        );
+    }
+})();
